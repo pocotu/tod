bb45de5d6aabbdd91d63e6cddecebfa4c40aeba7
pocotu <learning1830@outlook.com>
2022-04-24 16:42:52 -0500

    logo windows

---
+++ Python/Scripts/PYTHON/turtle/logo_windows.py
@@ -0,0 +1,28 @@
+import turtle
+
+t = turtle
+t.speed(1)
+t.bgcolor("black")
+t.penup()
+t.goto(-50, 60)
+t.pendown()
+t.color("green")
+turtle.begin_fill()
+turtle.goto(100, 100)
+turtle.goto(100, -100)
+#dibujar logo windows
+turtle.goto(-50, -60)
+turtle.goto(-50, 60)
+turtle.end_fill()
+turtle.color('black')
+turtle.goto(15, 100)
+# cortar en dos partes iguales
+turtle.color('black')
+turtle.width(10)
+turtle.goto(15, -100)
+turtle.penup()
+turtle.goto(100, 0)
+turtle.penup()
+turtle.pendown()
+turtle.goto(-100, 0)
+t.done()
