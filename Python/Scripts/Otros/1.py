1fdfa564b86ec72fff96856a462422ff241502f4
pocotu <learning1830@outlook.com>
2022-08-19 15:10:14 -0500

    Hola

--- Python/Scripts/Otros/1.py
+++ Python/Scripts/Otros/1.py
@@ -1,9 +1,11 @@
 s = input("Ingrese una cadena: ")
 
+# funcion palindrome
 def es_palindromo(s):
+    # agrega el texto 
     return s == s[::-1]
 
-print(es_palindromo(s))
+#print(es_palindromo(s))
 
 if s == es_palindromo(s):
     print("Es palindromo")
