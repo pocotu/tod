e9fb0ecf3e7e87011399ad2f927591887539f14a
pocotu <learning1830@outlook.com>
2022-10-19 08:11:24 -0500

    U

--- Python/Ing eco/2.py
+++ Python/Ing eco/2.py
@@ -1,6 +1,3 @@
-'''
-funcion para calcular el interes compuesto, 
-'''
 
 # Interes simple mensual
 def interes_S_M(monto, interes, tiempo):
@@ -12,66 +9,85 @@ def interes_S_M(monto, interes, tiempo):
 def interes_C_M(monto, interes, tiempo):
     for i in range(1, tiempo+1):
         monto = monto * (1 + interes / 100)
-        print("Monto total del {} mes es: {:.2f}".format(i, monto))
+        print("Monto total del {} mes: {:.2f}".format(i, monto))
         i += 1
     print("")
     return monto
 
+# Interes simple anual
+def interes_S_A(monto, interes, tiempo):
+    interes_M = interes / 12
+    interes =  monto * (interes_M / 100) * tiempo
+    monto = monto + interes
+    return monto
+
+# Interes compuesto anual
+def interes_C_A(monto, interes, tiempo):
+    interes_M = interes / 12
+    for i in range(1, tiempo+1):
+        monto = monto * (1 + interes_M / 100)
+        print("Monto total del {} mes: {:.2f}".format(i, monto))
+        i += 1
+    print("")
+    return monto
 
 def main():
     ## Menu ##
+    print("")
     print("1. Interes simple mensual")
     print("2. Interes compuesto mensual")
-    print("3. Salir")
+    print("3. Interes simple anual")
+    print("4. Interes compuesto anual")
+    print("5. Salir")
     print("")
-    opcion = int(input("Ingrese la opcion: "))
-    while 1< opcion < 3:
+    opcion = int(input("Ingrese opcion: "))
+    print("")
+    while 0 < opcion < 5:
         monto = float(input("Ingrese el monto del prestamo: "))
-        while monto < 0:
+        while monto <= 0:
             print("El monto debe ser mayor a 0")
             monto = float(input("Ingrese el monto del prestamo: "))
-        interes = float(input("Ingrese el interes anual: "))
-        while interes < 0:
+        interes = float(input("Ingrese el interes mensual: "))
+        while interes <= 0:
             print("El interes debe ser mayor a 0")
-            interes = float(input("Ingrese el interes anual: "))
-        tiempo = int(input("Ingrese el tiempo en meses: "))
+            interes = float(input("Ingrese el interes mensual: "))
+        tiempo = int(input("Ingrese el tiempo: "))
+        while tiempo <= 0:
+            print("El tiempo debe ser mayor a 0")
+            tiempo = int(input("Ingrese el tiempo: "))
         print("=====================================")
         while tiempo < 0:
             print("El tiempo debe ser mayor a 0")
-            tiempo = int(input("Ingrese el tiempo en meses: "))
+            tiempo = int(input("Ingrese el tiempo: "))
         if opcion == 1:
             monto_t = interes_S_M(monto, interes, tiempo)
+            print("Interes simple mensual")
             print("El monto total es: {:.2f}".format(monto_t))
         elif opcion == 2:
             monto_t = interes_C_M(monto, interes, tiempo)
+            print("Interes compuesto mensual")
+            print("El monto total es: {:.2f}".format(monto_t))
+        elif opcion == 3:
+            monto_t = interes_S_A(monto, interes, tiempo)
+            print("Interes simple anual")
+            print("El monto total es: {:.2f}".format(monto_t))
+        elif opcion == 4:
+            monto_t = interes_C_A(monto, interes, tiempo)
+            print("Interes compuesto anual")
             print("El monto total es: {:.2f}".format(monto_t))
         else:
-            print("Opcion incorrecta")
-            print("Ingrese la opcion: ")
+            print("Fin del programa")
+
+        print("=====================================")
         print("")
         print("1. Interes simple mensual")
         print("2. Interes compuesto mensual")
-        print("3. Salir")
+        print("3. Interes simple anual")
+        print("4. Interes compuesto anual")
+        print("5. Salir")
         print("")
-        opcion = int(input("Ingrese la opcion: "))
+        opcion = int(input("Ingrese opcion: "))
+        print("")
     print("Fin del programa")
         
-    #monto = float(input("Ingrese el monto del prestamo: "))
-    #while monto < 0:
-    #    print("El monto debe ser mayor a 0")
-    #    monto = float(input("Ingrese el monto del prestamo: "))
-    #interes = float(input("Ingrese el interes anual: "))
-    #while interes < 0:
-    #    print("El interes debe ser mayor a 0")
-    #    interes = float(input("Ingrese el interes anual: "))
-    #tiempo = int(input("Ingrese el tiempo en meses: "))
-    #while tiempo < 0:
-    #    print("El tiempo debe ser mayor a 0")
-    #    tiempo = int(input("Ingrese el tiempo en meses: "))
-    #
-    #print("=======================================")
-    #print("El monto total a devolver es: {:.2f}".format(interes_C_M(monto, interes, tiempo)))
-    #print("=======================================")
-    #print("El monto total a devolver es: {:.2f}".format(interes_S_M(monto, interes, tiempo)))
-
 main()
