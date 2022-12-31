9bc7755e68320bd63f2b1dc573cdc69cbfe13809
pocotu <learning1830@outlook.com>
2022-12-30 21:46:26 -0500

    .

--- Python/Progra 1/1era/Practica/nuevo.py
+++ Python/Progra 1/1era/Practica/nuevo.py
@@ -7,3 +7,18 @@ def triangulo(n):
 triangulo(5)
 
 # comentario
+
+# que imprima un triangulo con asteriscos de n filas
+
+# Path: Python\Progra 1\1era\Practica\nuevo.py
+
+def triangulo(n):
+    for i in range(1,n+1):
+        print("*"*i)
+
+def main():
+    n = int(input("Ingrese el numero de filas: "))
+
+    triangulo(n)
+
+main()
