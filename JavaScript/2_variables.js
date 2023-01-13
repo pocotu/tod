7a9cca6a1ad1c772069e965d58d7465122f0806d
pocotu <learning1830@outlook.com>
2023-01-13 13:46:42 -0500

    javs

---
+++ JavaScript/2_variables.js
@@ -0,0 +1,67 @@
+/**
+ * Variables: son pequeñas cajas donde podemos guardar datos
+ * 
+ * 3 formas de defenir variables:
+ * 
+ * - var 
+ * - let 
+ * - const
+ */
+
+// variables con VAR
+var nombre = 'Pedro'; //
+console.log(nombre);
+
+var edad = 27;
+console.log(edad);
+
+var persona = {
+    nombre: 'Pedro',
+    apellido: 'Arguello',
+    edad: '27',
+    direccion: {
+        calle: 'Buena Vista',
+        numero_de_casa: 4,
+    },
+    ciudades_visitadas: [
+        'Bogota', 'Medellin', 'Cali'
+    ]
+}
+
+var ciudad;
+ciudad = 'Lima';
+ciudad = 'Guayaquil'
+ciudad = 12;
+console.log(ciudad);
+
+// variables con LET
+
+let nombre2 = 'Thalia';
+console.log(nombre2);
+
+{
+    let saludo = "Hola"; // let solo existe dentro del bloque de codigo
+    console.log(saludo);
+
+}
+
+console.log(saludo);
+
+{
+    var saludo = "Hola"; // var existe fuera del bloque de codigo
+    console.log(saludo);
+
+}
+
+console.log(saludo);
+
+
+// variables con CONST
+const PI = 3.1416;
+//PI = 3.14; // no se puede reasignar el valor de una constante
+console.log(PI);
+
+
+var nombre = 'Pedro';
+var saludo2 = `Hola ${nombre}`; // template string
+console.log(saludo);
