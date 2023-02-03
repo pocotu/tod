c27059dd1fccaf4d6edbebd28cc12ef76f877a81
pocotu <learning1830@outlook.com>
2023-02-03 17:39:49 -0500

    jav

--- JavaScript/CurJS/Fundamentos/02_variables.js
+++ JavaScript/CurJS/Fundamentos/02_variables.js
@@ -1,15 +1,17 @@
-/**
- * Variables: son pequeñas cajas donde podemos guardar datos
- * 
- * 3 formas de defenir variables:
- * 
- * - var: se puede reasignar el valor de la variable y se puede usar fuera del bloque de codigo donde se definió
- * 
- * - let: se puede reasignar el valor de la variable pero solo dentro del bloque de codigo donde se definió
- * 
- * CONST: no se puede reasignar el valor de la variable, no podemos cambiar el valor de la variable
- * por fuera de la funcion donde se definió
- */
+/*
+Variables: son pequeñas cajas donde podemos guardar datos
+
+3 formas de defenir variables:
+
+var: se puede reasignar el valor de la variable y se puede usar fuera 
+del bloque de codigo donde se definió
+
+let: se puede reasignar el valor de la variable pero solo dentro 
+del bloque de codigo donde se definió
+
+CONST: no se puede reasignar el valor de la variable, no podemos cambiar 
+el valor de la variable por fuera de la funcion donde se definió
+*/
 
 // variables con VAR
 var nombre = 'Pedro'; //
--- JavaScript/CurJS/Fundamentos/Ejercicios/2_Control_de_flujo.js
+++ JavaScript/CurJS/Fundamentos/Ejercicios/2_Control_de_flujo.js
@@ -3,15 +3,61 @@
 El programa debe ser escrito dos veces, una con la declaración if else y otra con la declaración switch
 */
 
+let dia = 'lunes'
 
+if (dia === 'lunes') {
+    console.log('monday');
+} else if (dia === 'martes') {
+    console.log('tuesday');
+} else if (dia === 'miercoles') {
+    console.log('Wednesday');
+} else if (dia === 'jueves') {
+    console.log('thursday');
+} else if (dia === 'viernes') {
+    console.log('friday');
+} else if (dia === 'sabado') {
+    console.log('saturday');
+} else if (dia === 'domingo') {
+    console.log('sunday');
+} else {
+    console.log('dia invalido');
+}
+
+let day = 'lunes'
+switch (day) {
+    case 'lunes':
+        console.log('monday');
+        break;
+    case 'martes':
+        console.log('tuesday');
+        break;
+    case 'miercoles':
+        console.log('wednesday');
+        break;
+    case 'jueves':
+        console.log('thursday');
+        break;
+    case 'viernes':
+        console.log('friday');
+        break;
+    case 'sabado':
+        console.log('saturday');
+        break;
+    default:
+        console.log('dia incorrecto');
+}
 
 /*
 2.Escribir un programa que según el total de la compra, se agregue un valor de envio.
 Si la compra es menor o igual a los $10 se agregará un gasto de envió de $3
-
 Si la compra es menor o igual a los $20 y mayor a los $10 se agregará un gasto de envío de $5
 Si la compra es menor o igual a los $50 y mayor a los $20 se agregará un gasto de envío de $7
 Si la compra supera los $50 el gasto de envío será gratuito
 
 El programa debe ser escrito dos veces, una con la declaración if else y otra con la declaración switch
 */
+
+var precio = 7;
+
+
+
