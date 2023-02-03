2941ad49ae54c6f8ca75debcb4f112210a89fa2f
pocotu <learning1830@outlook.com>
2023-02-02 20:21:41 -0500

    JS

---
+++ JavaScript/CurJS/Fundamentos/Ejercicios/2_Control_de_flujo.js
@@ -0,0 +1,17 @@
+/*
+1.Escribir un programa que según el día de la semana ingresado proporcione el día en inglés.
+El programa debe ser escrito dos veces, una con la declaración if else y otra con la declaración switch
+*/
+
+
+
+/*
+2.Escribir un programa que según el total de la compra, se agregue un valor de envio.
+Si la compra es menor o igual a los $10 se agregará un gasto de envió de $3
+
+Si la compra es menor o igual a los $20 y mayor a los $10 se agregará un gasto de envío de $5
+Si la compra es menor o igual a los $50 y mayor a los $20 se agregará un gasto de envío de $7
+Si la compra supera los $50 el gasto de envío será gratuito
+
+El programa debe ser escrito dos veces, una con la declaración if else y otra con la declaración switch
+*/
