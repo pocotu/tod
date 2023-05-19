ab5e714a12f3e540d0a9c07a6315237ac2d08ba6
pocotu <learning1830@outlook.com>
2023-05-19 15:35:26 -0500

    burbuja

--- C#/progra2/reloj/Program.cs
+++ C#/progra2/reloj/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Diagnostics;
 
 public class Ejemplo
@@ -30,15 +30,24 @@ public class Ejemplo
         return f_sig;
     }
 
+    // Suma de numeros
+    public static int suma_numeros(int n)
+    {
+        int suma = 0;
+        for (int i = 1; i <= n; i++)
+        {
+            suma = suma + i;
+        }
+        return suma;
+    }
     public static void Main()
     {
         // Inicializamos el reloj
         Stopwatch stopwatch = new Stopwatch();
-        /////////////////////////////////////////////////
-        
-        int [] ln = {10, 20, 30, 40, 50,
-                    100, 200,300, 400, 500,
-                    1000, 2000, 3000, 5000,
+
+        int [] ln = {10, 20, 30, 40, 50, 
+                    100, 200,300, 400, 500, 
+                    1000, 2000, 3000, 5000, 
                     10000, 20000, 50000, 100000};
 
         Console.WriteLine("#### Factorial iterativo ####");
@@ -46,7 +55,7 @@ public class Ejemplo
             stopwatch.Start();
             Console.WriteLine(fac_iterativo(i));
             stopwatch.Stop();
-            Console.WriteLine("El tiempo transcurrido es {0} ms",
+            Console.WriteLine("El tiempo transcurrido es {0} ms", 
                 stopwatch.ElapsedMilliseconds/1000.0);
         }
 
@@ -55,30 +64,8 @@ public class Ejemplo
             stopwatch.Start();
             Console.WriteLine(fac_recursivo(j));
             stopwatch.Stop();
-            Console.WriteLine("El tiempo transcurrido es {0} ms",
-            stopwatch.ElapsedMilliseconds/1000.0);
+            Console.WriteLine("El tiempo transcurrido es {0} ms", 
+                stopwatch.ElapsedMilliseconds/1000.0);
         }
-        
-        /////////////////////////////////////////////////
-        
-        // Código a medir para el factorial recursivo
-        stopwatch.Start();
-        int resultado1 = fac_recursivo(50);
-        stopwatch.Stop();
-        Console.WriteLine("El resultado del factorial recursivo es: {0}", resultado1);
-        Console.WriteLine("El tiempo de ejecución para el factorial recursivo es: {0} ms", stopwatch.ElapsedMilliseconds);
-        Console.WriteLine();
-
-        // Reinicializamos el reloj
-        stopwatch.Reset();
-
-        // Código a medir para el factorial iterativo
-        stopwatch.Start();
-        int resultado2 = fac_iterativo(50);
-        stopwatch.Stop();
-        Console.WriteLine("El resultado del factorial iterativo es: {0}", resultado2);
-        Console.WriteLine("El tiempo de ejecución para el factorial iterativo es: {0} ms", stopwatch.ElapsedMilliseconds);
-        /////////////////////////////////////////////////
-        
     }
 }
