830f3eb96e63b863cd4701133122d97607469b07
pocotu <learning1830@outlook.com>
2023-05-23 17:20:41 -0500

    css

---
+++ Python/AED/Csharp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace appPOOEjercicio1
+
+{
+    Class Program
+    {
+        static void Main(string[] args)
+        {
+            // crear 2 objetos de la clase CPunto (instanciar)
+            CPunto punto1 = new CPunto();
+            CPunto punto2 = new CPunto(5, 10);
+
+            // Imprimir los valores de los atributos de los objetos
+            Console.WriteLine("punto1 = ("+punto1.X+","+punto1.Y+")");
+            console.ReadLine();
+        }
+    }
+}
