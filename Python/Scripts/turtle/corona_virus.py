fbb109d3043b15f4c1a87ec42c1b707775a79d00
pocotu <learning1830@outlook.com>
2023-05-24 22:06:52 -0500

    dos

--- Python/Scripts/turtle/corona_virus.py
+++ Python/Scripts/turtle/corona_virus.py
@@ -3,10 +3,15 @@ from turtle import *
 speed(10)
 color('yellow')
 bgcolor('black')
-i = 100 # numero de vueltas
+i = 200 # numero de vueltas
 while i>0:
+    # left(i) hace que la tortuga gire a la izquierda
     left(i)
-    forward(i*3)
+    # forward(i*3) hace que la tortuga avance
+    forward(i*1)
+
+    # imprimir en que numero de vuelta esta de cada vuelta
+    print(i)
     i = i-1
 
-# mantiene la ventana abierta
+
