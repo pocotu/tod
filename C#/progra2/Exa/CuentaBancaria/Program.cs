9feb60141f5ce4e3a3a632f6abaf3eb813a25665
pocotu <learning1830@outlook.com>
2023-06-19 10:08:06 -0500

    examen

---
+++ C#/progra2/Exa/CuentaBancaria/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+class CuentaBancaria
+{
+    // Atributos
+    private string numeroCuenta;
+    private string titular;
+    private decimal saldo;
+    
+    // Constructor 
+    public CuentaBancaria(string numeroCuenta, string titular)
+    {
+        this.numeroCuenta = numeroCuenta;
+        this.titular = titular;
+        this.saldo = 0;
+    }
+
+    // Metodo para depositar
+    public void Depositar(decimal monto)
+    {
+        saldo += monto;
+    }
+
+    // Metodo para retirar
+    public void Retirar(decimal monto)
+    {
+        saldo -= monto;
+    }
+    // Metodo para mostrar saldo
+    public decimal MostrarSaldo()
+    {
+        return saldo;
+    }
+}
+
+// Clase principal
+class Program
+{
+    static void Main(string[] args)
+    {
+        CuentaBancaria cuenta = new CuentaBancaria("123456789", "Juan Perez");
+        Console.WriteLine("Saldo inicial: " + cuenta.MostrarSaldo());
+        cuenta.Depositar(1000);
+        Console.WriteLine("Despues de depositar: " + cuenta.MostrarSaldo());
+        cuenta.Retirar(500);
+        Console.WriteLine("Despues de retirar: " + cuenta.MostrarSaldo());
+    }
+}
