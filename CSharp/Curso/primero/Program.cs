694d7c8d55dd6bf96ad15ce4eec6f28ec39f41a7
pocotu <learning1830@outlook.com>
2023-06-26 10:10:39 -0500

    n

--- CSharp/Curso/primero/Program.cs
+++ CSharp/Curso/primero/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace primero
+{
+    // en esta parte se declara la 
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola a todos");
+            Console.ReadKey();
+        }
+    }
+}
