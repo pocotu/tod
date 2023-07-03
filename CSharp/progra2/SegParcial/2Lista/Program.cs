2027e1b523ed23f6f92f0c43b645c587e91a73af
pocotu <learning1830@outlook.com>
2023-07-03 10:42:18 -0500

    class

---
+++ CSharp/progra2/SegParcial/2Lista/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+class Program
+{
+    static void Main (string[] args)
+    {
+        // crear un lista de enteros
+        List <int> numeros = new List<int>();
+
+        // Agregar elementos a la lista
+        numeros.Add(10);
+        numeros.Add(20);
+        numeros.Add(30);
+        numeros.Add(40);
+
+        // Acceder a los elementos de la lista
+        Console.WriteLine("Elementos de la lista:");
+        foreach (int numero in numeros)
+        {
+            Console.WriteLine(num);
+        }
+        
+        // Optener la cantidad de elementos de la lista
+        int cantidadElementos = numeros.Count;
+        Console.WriteLine("Cantidad de elementos en la lista: " + cantidadElementos);
+
+        // Verificar si la lista contiene un elemento especifico
+        int numeroBuscado = 30;
+        if (numeros.Contains(numeroBuscado))
+        {
+            Console.WriteLine("La lista contiene el numero " + numeroBuscado);
+        }
+        else
+        {
+            Console.WriteLine("La lista no contiene el numero " + numeroBuscado);
+        }
+
+        // Eliminar un elemento de la lista
+        numeros.Remove(20);
+        Console.WriteLine("Elementos de la lista despues de eliminar el numero 20:");
+        foreach (int numero in numeros)
+        {
+            Console.WriteLine(num);
+        }
+
+        // Limpiar todos los elementos de la lista
+        numeros.Clear();
+    }
+}
---
+++ CSharp/progra2/SegParcial/CLista/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lista
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Crear una lista de enteros
+            List <int> numeros = new List<int>();
+
+            // Agregar elementos a la lista
+            numeros.Add(10);
+            numeros.Add(20);
+            numeros.Add(30);
+            numeros.Add(40);
+
+            // Acceder a los elementos de la lista
+            Console.WriteLine("Elementos de la lista:");
+            foreach (int n in numeros)
+            {
+                Console.WriteLine(n);
+            }
+
+            // Obtener la cantidad de elementos en la lista
+            int cantidadElementos = numeros.Count;
+            Console.WriteLine("Cantidad de elementos en la lista: " + cantidadElementos);
+
+            // Verificar Si la lista contiene un elemento especifico
+            int numeroBuscado = 30;
+            if (numeros.Contains(numeroBuscado))
+            {
+                Console.WriteLine("La lista contiene el numero: " + numeroBuscado);
+            }
+            else
+            {
+                Console.WriteLine("La lista no contiene el numero: " + numeroBuscado);
+            }
+
+            // Eliminar un elemento
+            int numeroEliminar = 20;
+            numeros.Remove(numeroEliminar);
+            Console.WriteLine("Elementos en la lista despues de eliminar al numero " + numeroEliminar);
+            foreach(int n in numeros)
+            {
+                Console.WriteLine(n);
+            }
+
+            // Limpiar todos los elementos de la lista
+            numeros.Clear();
+        }
+    }
+}
