81cb1e070aabc23ec2a835e51fe8a389d873e251
pocotu <learning1830@outlook.com>
2024-01-06 03:10:36 -0500

    sali

--- Competitiva/1_busqueda_binaria.cpp
+++ Competitiva/1_busqueda_binaria.cpp
@@ -10,18 +10,25 @@ public:
         while (low <= high) {
             int mid = low + (high - low) / 2;
 
+            std::cout << "Comprobando elemento en el indice " << mid << std::endl;
+
             if (nums[mid] == target) {
+                std::cout << "Objetivo encontrado en el indice " << mid << std::endl;
                 return mid;
             } else if (nums[mid] < target) {
+                std::cout << "El objetivo es mayor, buscando en la mitad derecha" << std::endl;
                 low = mid + 1;
             } else {
+                std::cout << "El objetivo es menor, buscando en la mitad izquierda" << std::endl;
                 high = mid - 1;
             }
         }
 
+        std::cout << "Objetivo no encontrado" << std::endl;
         return -1;
     }
 };
+
 int main() {
     std::vector<int> nums1 = {1, 3, 5, 7, 9};
     int target1 = 5;
